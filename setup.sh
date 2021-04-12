3f0567b7fd6cc08b6561ea116b6d78e47ef06c4d
Kevin Carter <kecarter@redhat.com>
2021-04-12 11:57:55 -0500

    Update setup script to be more dynamic
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-dnf install -y https://trunk.rdoproject.org/centos8/component/tripleo/current/python3-tripleo-repos-0.1.1-0.20210118183911.2cfaa48.el8.noarch.rpm
+#!/usr/bin/env bash
+
+# Enable powertools repo
+sed -i 's/^enabled.*/enabled=1/' /etc/yum.repos.d/*PowerTools.repo
+
+dnf -y install lynx
+
+TRIPLEO_REPOS=$(lynx -dump -hiddenlinks=listonly https://trunk.rdoproject.org/centos8/component/tripleo/current/ | awk '/python3-tripleo-repos.*rpm$/ {print $2}')
+
+dnf install -y ${TRIPLEO_REPOS}
+
 tripleo-repos --stream -b master current-tripleo ceph
 
 dnf install -y zeromq python3 etcd
@@ -7,8 +17,12 @@ dnf install -y zeromq python3 etcd
 dnf install -y gcc
 
 # Create development workspace
-python -m venv /opt/director
+python3 -m venv /opt/director
 /opt/director/bin/pip install --upgrade pip setuptools wheel
+/opt/director/bin/pip install "$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 
-# Python packages will need a corresponding RPM
-/opt/director/bin/pip install pyzmq etcd3
+echo -e "\nDirector is setup and installed within [ /opt/director ]"
+echo "Activate the venv or run director directly."
+echo "Director can be installed as a service using the following command(s):"
+echo "director-client-systemd"
+echo "director-server-systemd\n"
