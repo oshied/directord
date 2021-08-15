44000297f8a384849610f5aaf922ffe32cad31ea
Kevin Carter <kevin@cloudnull.com>
2021-08-15 12:29:03 -0500

    move test rpm workflows (#89)
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- contrib/rpm-build.sh
+++ contrib/rpm-build.sh
@@ -4,7 +4,15 @@ set -e
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 CONTAINER_NAME=localhost/centos8-builder
 
-tar -czf /tmp/directord.tar.gz ${SCRIPT_DIR}/../../directord
-mv /tmp/directord.tar.gz ${SCRIPT_DIR}/container-build/
+pushd ${SCRIPT_DIR}/../
+  name=${PWD##*/}
+  pushd ..
+  tar -czf /tmp/directord.tar.gz "${name}"
+  mv /tmp/directord.tar.gz ${SCRIPT_DIR}/container-build/
+  popd
+popd
+
+export RELEASE_VERSION=$(awk -F'"' '/version/ {print $2}' ${SCRIPT_DIR}/../../directord/meta.py)
+
 buildah bud -t ${CONTAINER_NAME} ${SCRIPT_DIR}/container-build
-podman run -it -v ${SCRIPT_DIR}:/home/builder/rpm:z ${CONTAINER_NAME}:latest
+podman run -it -v ${SCRIPT_DIR}:/home/builder/rpm:z ${CONTAINER_NAME}:latest --env RELEASE_VERSION="${RELEASE_VERSION}"
