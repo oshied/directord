9c5e7798786240aa43d3a11e91dd664009ab50c5
Kevin Carter <kecarter@redhat.com>
2021-08-15 10:35:13 -0500

    add local source builds
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- contrib/rpm-build.sh
+++ contrib/rpm-build.sh
@@ -4,5 +4,7 @@ set -e
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 CONTAINER_NAME=localhost/centos8-builder
 
-buildah bud -t $CONTAINER_NAME $SCRIPT_DIR/container-build
-podman run -it -v $SCRIPT_DIR:/home/builder/rpm:z $CONTAINER_NAME:latest
+tar -czf /tmp/directord.tar.gz ${SCRIPT_DIR}/../../directord
+mv /tmp/directord.tar.gz ${SCRIPT_DIR}/container-build/
+buildah bud -t ${CONTAINER_NAME} ${SCRIPT_DIR}/container-build
+podman run -it -v ${SCRIPT_DIR}:/home/builder/rpm:z ${CONTAINER_NAME}:latest
