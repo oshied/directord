8eee524030993d3db499fb2cbf4815aa2ab0a8d5
Kevin Carter <kecarter@redhat.com>
2021-08-16 17:11:16 -0500

    add rpm install in prod setup
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- tools/prod-setup.sh
+++ tools/prod-setup.sh
@@ -7,29 +7,44 @@ fi
 
 set -eo
 
+function get_latest_release() {                                                                                                                                                                                                                                130 ↵
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
 . /etc/os-release
 if [[ ${ID} == "rhel" ]] || [[ ${ID} == "centos" ]]; then
   dnf -y install https://www.rdoproject.org/repos/rdo-release.el8.rpm
-  PACKAGES="git python3 python3-tenacity python3-tabulate python3-zmq python3-pyyaml python3-jinja2 zeromq libsodium"
-  dnf -y install ${PACKAGES}
+  PACKAGES="git wget python3 python3-tenacity python3-tabulate python3-zmq python3-pyyaml python3-jinja2 zeromq libsodium"
+  COMMAND="dnf -y install"
+  eval "${COMMAND} ${PACKAGES}"
 elif [[ ${ID} == "fedora" ]]; then
-  PACKAGES="git python3 python3-ssh2-python python3-tenacity python3-tabulate python3-zmq python3-pyyaml python3-jinja2 zeromq libsodium python3-diskcache"
-  dnf -y install ${PACKAGES}
+  PACKAGES="git wget python3 python3-ssh2-python python3-tenacity python3-tabulate python3-zmq python3-pyyaml python3-jinja2 zeromq libsodium python3-diskcache"
+  COMMAND="dnf -y install"
+  eval "${COMMAND} ${PACKAGES}"
 elif [[ ${ID} == "ubuntu" ]]; then
   PACKAGES="git python3-all python3-venv python3-tabulate python3-zmq python3-yaml python3-jinja2"
   apt update
-  apt -y install ${PACKAGES}
+  COMMAND="apt -y install"
+  eval "${COMMAND} ${PACKAGES}"
 else
   echo -e "Failed unknown OS"
   exit 99
 fi
 
-python3 -m venv --system-site-packages /opt/directord
-/opt/directord/bin/pip install --upgrade pip setuptools wheel
-/opt/directord/bin/pip install directord
+RELEASE="$(get_latest_release cloudnull/directord)"
 
-echo -e "\nDirectord is setup and installed within [ /opt/directord ]"
+mkdir -p ~/directord-RPMS
+pushd ~/directord-RPMS
+  for item in python3-directord-0.7.3-1.el8.noarch.rpm python3-diskcache-5.2.1-1.el8.noarch.rpm python3-ssh2-python-0.26.0-1.el8.x86_64.rpm; do
+    wget https://github.com/cloudnull/directord/releases/download/${RELEASE}/${item}
+  done
+  eval "${COMMAND} *.rpm"
+popd
+
+echo -e "\nDirectord is setup and installed within [ /usr/bin ]"
 echo "Activate the venv or run directord directly."
 echo "Directord can be installed as a service using the following command(s):"
-echo "/opt/directord/bin/directord-client-systemd"
-echo -e "/opt/directord/bin/directord-server-systemd\n"
+echo "/usr/bin/directord-client-systemd"
+echo -e "/usr/bin/directord-server-systemd\n"
