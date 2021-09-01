e3af0fd671a42f471009052e934f6d86b56f9ea6
Kevin Carter <kecarter@redhat.com>
2021-09-01 10:59:17 -0500

    Simplify dev|prod scripts
    Changes make the dev and prod scripts / orchestrations easier to grok.
    
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- tools/dev-setup.sh
+++ tools/dev-setup.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+#   Copyright Peznauts <kevin@cloudnull.com>. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
 set -evo
 
 VENV_PATH="${1:-/opt/directord}"
@@ -60,6 +73,25 @@ if [ -z "${CLONE_PATH}" ] || [ ! -d "${CLONE_PATH}" ] ; then
   ${VENV_PATH}/bin/pip install --upgrade --pre directord[all]
 else
   ${VENV_PATH}/bin/pip install --upgrade ${CLONE_PATH}[all]
+fi
+
+# Create basic development configuration
+mkdir -p /etc/directord /etc/directord/private_keys /etc/directord/public_keys
+${VENV_PATH}/bin/python3 <<EOC
+import yaml
+try:
+    with open('/etc/directord/config.yaml') as f:
+        config = yaml.safe_load(f)
+except FileNotFoundError:
+    config = dict()
+config["curve_encryption"] = True
+config["debug"] = True
+with open('/etc/directord/config.yaml', 'w') as f:
+    f.write(yaml.safe_dump(config, default_flow_style=False))
+EOC
+
+if [ ! -f "/etc/directord/private_keys/server.key_secret" ]; then
+  ${VENV_PATH}/bin/directord manage --generate-keys
 fi
 
 if [ "${SETUP}" = true ]; then
