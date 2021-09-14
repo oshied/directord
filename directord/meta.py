15499e8ceb9bd1e8aeecb815d77063476701d713
Kevin Carter <kecarter@redhat.com>
2021-09-13 22:04:50 -0500

    rev 0.9.5
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- directord/meta.py
+++ directord/meta.py
@@ -12,7 +12,7 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 __author__ = "Kevin Carter"
 __email__ = "kevin@cloudnull.com"
 __driver_options__ = ["zmq"]
